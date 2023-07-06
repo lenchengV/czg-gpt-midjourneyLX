8006ca3a0862927c9df7dea26eca975e481cb8bc
1040342522@qq.com <1040342522@qq.com>
2023-07-06 09:24:11 +0800

    refalsh  title

--- app/constant.ts
+++ app/constant.ts
@@ -1,10 +1,10 @@
-export const OWNER = "Licoy";
-export const REPO = "ChatGPT-Midjourney";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const OWNER = "czg";
+export const REPO = "czg";
+export const REPO_URL = `http://www.autovalley1898.com/${OWNER}/${REPO}`;
+export const ISSUE_URL = `http://www.autovalley1898.com//${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const FETCH_COMMIT_URL = `https://api.autovalley1898.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
+export const FETCH_TAG_URL = `https://api.autovalley1898.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export enum Path {
